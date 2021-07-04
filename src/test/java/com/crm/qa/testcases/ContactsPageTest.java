76db62f47885a163ca8bbc8c02a63ddab9658f88
alaguraja-git <alagu.dublin@gmail.com>
2021-07-04 20:12:34 +0530

    updated scripts and added new scripts

--- src/test/java/com/crm/qa/testcases/ContactsPageTest.java
+++ src/test/java/com/crm/qa/testcases/ContactsPageTest.java
@@ -73,13 +73,13 @@ public class ContactsPageTest extends TestBase{
 	}
 	
 	
-//	@Test(priority=4, dataProvider="getCRMTestData")
-//	public void validateCreateNewContact(String title, String firstName, String lastName, String company){
-//		homePage.clickOnNewContactLink();
-//		//contactsPage.createNewContact("Mr.", "Tom", "Peter", "Google");
-//		contactsPage.createNewContact(title, firstName, lastName, company);
-//		
-//	}
+	@Test(priority=4, dataProvider="getCRMTestData")
+	public void validateCreateNewContact(String title, String firstName, String lastName, String company){
+		homePage.clickOnNewContactLink();
+		//contactsPage.createNewContact("Mr.", "Tom", "Peter", "Google");
+		contactsPage.createNewContact(title, firstName, lastName, company);
+		
+	}
 	
 	
 
